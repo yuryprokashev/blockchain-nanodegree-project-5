f5b58367560aabe90dcf835a6183ec2e028dad5c
Yury Prokashev <yury.prokashev@gmail.com>
2018-10-13 19:57:48 +0300

    Unit tests for ERC721Mintable.sol

---
+++ test/DescribeBlockTemplate.js
@@ -0,0 +1,46 @@
+describe("1. Transactions that mutate the contract state", async function() {
+    describe("<index> <Name> Transaction.", async function () {
+
+        describe("Access Control Errors", async function() {
+            it("Throws an Error, when transaction sender does not have <valid value>", async function () {
+
+            });
+        });
+
+        describe("Successful Case", async function () {
+            it('<Name> Transaction successful', async function () {
+
+            });
+        });
+
+        describe("Invalid Input Errors", async function () {
+            it("Throws an Error, when <input param> = <invalid value>", async function () {
+
+            });
+        });
+    });
+});
+
+
+describe("2. Transactions that read the contract state", async function() {
+    describe("<index> <Name> Transaction.", async function () {
+
+        describe("Access Control Errors", async function() {
+            it("Does not have access restrictions", async function () {
+
+            });
+        });
+
+        describe("Successful Case", async function () {
+            it('<Name> Transaction successful', async function () {
+
+            });
+        });
+
+        describe("Invalid Input Errors", async function () {
+            it("Throws an Error, when <input param> = <invalid value>", async function () {
+
+            });
+        });
+    });
+});
